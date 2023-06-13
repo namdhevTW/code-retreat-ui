aa56823a8f96d80bb098a72b023ff7e42de6ef5d
Kalarani <lkalarani@gmail.com>
2023-06-13 07:28:50 +0530

    Updates HTML to have elements related to ping-pong API

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import { renderChart } from "./chart.js";
-import { getReadings, groupByDay, sortByTime } from "./reading";
+import { ping } from './client';
+import axios from 'axios';
 
-const readings = await getReadings();
-renderChart(sortByTime(groupByDay(readings)).slice(-30));
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Request-Method'] = ['POST', 'GET', 'OPTIONS'];
+
+document.getElementById("ping").onclick = async function(){
+    const message = document.getElementById("message").value;
+    document.getElementById("response").innerText = message;
+    await ping(message);
+}
